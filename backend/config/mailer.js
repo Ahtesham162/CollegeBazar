3e41b4c97e54d54939f008cf456620504bf2a9bf
Ahtesham162 <ahteshamm682@gmail.com>
2026-08-21 02:06:20 +0530

    Switch email sending from Gmail SMTP to Brevo API

--- backend/config/mailer.js
+++ backend/config/mailer.js
@@ -1,34 +1,36 @@
-import nodemailer from 'nodemailer';
+import axios from 'axios';
 import dotenv from 'dotenv';
 
 dotenv.config();
-
-const transporter = nodemailer.createTransport({
-  service: 'Gmail',
-  auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS // App password and not the regular password
-  }
-});
 
 const sendMail = async (to, subject, html, text = '') => {
 
   try {
 
-    const info = await transporter.sendMail({
-      from: `"CollegeBazaar" <${process.env.EMAIL_USER}>`,
-      to,
-      subject,
-      html,
-      text
-    });
+    const response = await axios.post(
+      'https://api.brevo.com/v3/smtp/email',
+      {
+        sender: { name: 'CollegeBazaar', email: process.env.BREVO_SENDER_EMAIL },
+        to: [{ email: to }],
+        subject,
+        htmlContent: html,
+        textContent: text || undefined
+      },
+      {
+        headers: {
+          'api-key': process.env.BREVO_API_KEY,
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        }
+      }
+    );
 
-    console.log('Email sent:', info.messageId);
-    return info;
+    console.log('Email sent:', response.data.messageId);
+    return response.data;
 
   } 
   catch (err) {
-    console.error('Error sending email:', err);
+    console.error('Error sending email:', err.response?.data || err.message);
     throw err;
   }
 
